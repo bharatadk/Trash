7f875a58a1b546411e4428d30cf3f5631aeb0882
Bharat Adhikari <51126350+bharatadk@users.noreply.github.com>
2023-03-06 22:22:15 +0545

    task1 and task2 code updated

--- app(python_anywhere).py
+++ app(python_anywhere).py
@@ -30,8 +30,10 @@ from dateutil import parser
 import json
 import csv
 import pandas as pd
-
 import socket
+import time
+import random
+from page_limiter import page_limiter
 
 app = Flask(__name__)
 
@@ -42,7 +44,7 @@ app.config["uploaded_files"] = []
 app.config["TEMP_NAME"] = []
 app.config["TEMP_Imagecode"] = ""
 app.config["Data"] = []
-
+app.config['is_PDF']  = False
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///Cordinates.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
@@ -504,6 +506,8 @@ def images(f):
     return send_file(os.path.join('/home/bharat7243/images',f))
 
 
+
+
 @app.route("/upload", methods=["GET", "POST"])
 @token_required
 @login_required
@@ -558,13 +562,16 @@ def upload():
                     data = Main(str(count) + ".jpg", file.filename, count, option)
                     print("up_data",data)
                     if len(data) == 0:
-                        break
+                        data=""
+                        continue
+
 
                     # app.config["Data"].append(data)
 
                     ###############################combined_data
                     combined_data = {}
                     id = str(count_img)
+
                     for record in data.values():
                         # id = record['id']
                         if id not in combined_data:
@@ -572,19 +579,46 @@ def upload():
                                 'folder_name': record['folder_name'],
                                 'filename': record['filename'],
                                 'Page_n': record['Page_n'],
-                                'id': id
+                                'id': id,
+
                             }
                         field_name = record['field_name']
+
+                        #original code
                         label_data = record['label_data'].strip()
+
+                        #########################################
+                        #temp code
+                        if record['Format'] == "Table":
+                            print("yes number.........")
+
+                            label_data = [record['label_data'].strip().replace(",","").replace("%","").replace("\n",", ").replace("=","")]
+                            # if label_data[0].split(',')
+                            contains_only_numbers = all(num.strip().isdigit() for num in label_data[0].split(','))
+
+                            if contains_only_numbers:
+                                # Convert list of strings to list of integers
+                                label_data = [int(num.strip()) for num in label_data[0].split(',')]
+                                print('int_list')
+                            else:
+                                label_data = [s.strip() for s in label_data[0].split(',') if s.strip()]
+
+                                print("List contains non-numeric elements.")
+                        #########################################
                         combined_data[id][field_name] = label_data
 
                     app.config["Data"].append(combined_data[id])
-                    print("combined_data:",combined_data)
                     count_img+=1
+                    if page_limiter(count_img):
+                        break
+                if page_limiter(count_img):
+                    break
 
                 # -----new code end
             elif extention in [".jpg", ".png", ".jpeg", ".tiff", ".tif"]:
+
                 page_count = 1
+
                 for f in os.listdir("./upload"):
 
                     data = MainImg(
@@ -592,8 +626,12 @@ def upload():
                     )
 
                     os.remove(os.path.join("./upload", f))
+
+                    ########################combined_data
                     combined_data = {}
                     id = str(count_img)
+
+
                     for record in data.values():
                         # id = record['id']
                         if id not in combined_data:
@@ -604,12 +642,14 @@ def upload():
                                 'id': id
                             }
                         field_name = record['field_name']
-                        label_data = record['label_data'].strip()
+                        label_data = record['label_data'].strip() or "nil"
                         combined_data[id][field_name] = label_data
 
                     app.config["Data"].append(combined_data[id])
-                    print("data",data)
                     count_img+=1
+                if page_limiter(count_img):
+                    break
+
 
         return redirect(url_for("download", token=[token]))
     try:
@@ -625,6 +665,7 @@ def upload():
     return render_template("upload.html", form=form, token=token, status=int(d.status))
 
 
+
 @app.route("/download", methods=["POST", "GET"])
 @token_required
 @login_required
@@ -633,24 +674,23 @@ def download():
     d = tbl_user.query.filter_by(id=current_user.id).first()
     Data = []
 
-    print(app.config["Data"])
     ############################# added this
     print("✅✅✅")
     data =app.config["Data"]
 
     df = pd.DataFrame(data)
-    print(df)
     data_json = df.to_json(orient="columns")
+
+    print(df)
 
     return render_template(
         "JsonData.html",
         tables=df.values.tolist(),
         columns=df.columns,
-        data =data_json,
+        data=data_json,
         token=token,
         status=int(d.status),
     )
-
 
 @app.route("/apply/<int:id>")
 @token_required
@@ -736,4 +776,3 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True, port=3000)
-
